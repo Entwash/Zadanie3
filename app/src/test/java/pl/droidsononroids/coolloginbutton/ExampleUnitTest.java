eb8146560e86c0053c3aa0ad50686ae562072346
JMichalowski <jk3105@wp.pl>
2016-07-27 10:31:36 +0200

    test

---
+++ app/src/test/java/pl/droidsononroids/coolloginbutton/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package pl.droidsononroids.coolloginbutton;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void love_isCorrect() throws Exception {
+        assertEquals("DoR <3", "DoR" + " " + "<3");
+    }
+}
